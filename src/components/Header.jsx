2f61814ddcbeab8e02306a3c9c1a8422b2eb6fda
lina98st <156576799+lina98st@users.noreply.github.com>
2025-12-25 16:03:49 +0100

    Initial React project with Vite

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return ( 
+        <header className="header">
+            <h1 className="links-header">Dental practice - Welcome</h1>
+        </header>
+    )
+}

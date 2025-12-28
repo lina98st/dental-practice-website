219751c26ece6679827205c71894415e69bc5a9a
lina98st <156576799+lina98st@users.noreply.github.com>
2025-12-28 15:59:58 +0100

    Fix layout and header navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,12 @@ export default function Header() {
     return ( 
         <header className="header">
             <h1 className="header-h1">Dental practice - Welcome</h1>
+                        <nav className="navigation">
+  <a href="#">Home</a> |
+  <a href="#">Contact</a> |
+  <a href="#">Location</a> |
+  <a href="#">Opening Hours</a>
+</nav>
         </header>
     )
 }

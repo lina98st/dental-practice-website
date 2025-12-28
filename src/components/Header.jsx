e0ecd51a81410559b90cef1cc76f4da07792f657
lina98st <156576799+lina98st@users.noreply.github.com>
2025-12-28 11:44:59 +0100

    add layout with header and footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return ( 
         <header className="header">
-            <h1 className="links-header">Dental practice - Welcome</h1>
+            <h1 className="header-h1">Dental practice - Welcome</h1>
         </header>
     )
 }

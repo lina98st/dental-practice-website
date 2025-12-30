64db6b2f86084b086fd2ca9cc6c2f345d70b2a1b
lina98st <156576799+lina98st@users.noreply.github.com>
2025-12-30 14:21:59 +0100

    Add carousel component with images

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,16 +1,11 @@
 //write function for carousel
-//array map für carousel
 //export carousel in die layout oder app.jsx
 //spread benutzen für map array in fuction?
-import Dentist1 from ".../assets/images/dentist1.jpg"
-import Dentist2 from ".../assets/images/dentist2.jpg"
-import Dentist3 from ".../assets/images/dentist3.jpg"
-import Dentist4 from ".../assets/images/dentist4.jpg"
-
-useState[carouselOff, carouselOn] = useState();
-//hier muss ein State für den Slide Index hin
-
-
+import { useState } from "react";
+import Dentist1 from "../assets/images/dentist1.jpg"
+import Dentist2 from "../assets/images/dentist2.jpg"
+import Dentist3 from "../assets/images/dentist3.jpg"
+import Dentist4 from "../assets/images/dentist4.jpg"
 
 let carouselImages = [
      { image: Dentist1 },
@@ -20,6 +15,9 @@ let carouselImages = [
     ]
 
 export default function Carousel () {
+const [currentIndex, setCurrentIndex] = useState(0);
+
+
     return (
         <div className="carousel-images">
             {carouselImages.map((item, i) => (
